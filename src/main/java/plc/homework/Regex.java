04e99b7658819133e5159083ddc6c800d6e22e88
scash1985 <s.cash@ufl.edu>
2024-09-07 23:27:14 -0400

    Regex and RegexTests modified

--- src/main/java/plc/homework/Regex.java
+++ src/main/java/plc/homework/Regex.java
@@ -11,8 +11,8 @@ public class Regex {
     public static final Pattern
             EMAIL = Pattern.compile("[A-Za-z0-9._\\-]+@[A-Za-z0-9-]*\\.[a-z]{2,3}"),
             EVEN_STRINGS = Pattern.compile("^(.{2}){5,10}$"),
-            INTEGER_LIST =  Pattern.compile("^\\[(\\s*\\d+\\s*(,\\s*\\d+\\s*)*)?\\]$"),
+            INTEGER_LIST =  Pattern.compile("^\\[\\s*\\]$|^\\[(\\s*\\d+\\s*(,\\s*\\d+\\s*)*)\\]$"),
             NUMBER = Pattern.compile("^[+-]?\\d+(\\.\\d+)?$"),
-            STRING = Pattern.compile("^\"(\\\\[bnrt'\"\\\\u]|[^\"\\\\])*\"$");
+            STRING = Pattern.compile("^\"(\\\\[bnrtf'\"\\\\u]|\\\\u[0-9a-fA-F]{4}|[^\"\\\\])*\"$");
 
 }
